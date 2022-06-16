5f62148182443e0114d73bd574a7fcdeca5b3b30
betty melaku <melakubetty@gmail.com>
2022-06-16 18:57:19 +0300

    betty commit

--- Web/Tests/Chrome_Tests/test_NavBar.py
+++ Web/Tests/Chrome_Tests/test_NavBar.py
@@ -1,27 +1,38 @@
 import allure
 import pytest
 from Web.Pages.navBar_Page import NavBarPage
-from Web.Utils.PreConditions.precondition_betty import Pre_Condition_Betty
+from Web.Utils.PreConditions.precondition_tsiona import Pre_Condition_Tsiona
 from Web.Locators.NavBar_Locators import NavBarLocators
 
 
-@pytest.mark.usefixtures('login_correctly')
-class TestNavBer(Pre_Condition_Betty):
+@pytest.mark.usefixtures('login')
+class TestNavBer(Pre_Condition_Tsiona):
 
     @allure.severity(allure.severity_level.CRITICAL)
     @allure.description("Validates links opening correctly")
-    def test_links(self, login_correctly):
+    def test_links(self):
 
         """Validates links opening correctly"""
         driver = self.driver
         headers = NavBarPage(driver)
-        headers.clicking(NavBarLocators.header1,NavBarLocators.list2,NavBarLocators.list_txt2)
+        headers.clicking(NavBarLocators.header1, NavBarLocators.list2, NavBarLocators.list_txt2)
 
     @allure.severity(allure.severity_level.CRITICAL)
     @allure.description("Validates menu buttons opening correctly")
-    def test_menuButtons(self, login_correctly):
+
+
+    def test_menuButtons(self):
 
         """Validates menu buttons opening correctly"""
         driver = self.driver
         headers = NavBarPage(driver)
-        headers.clicking(NavBarLocators.header2,NavBarLocators.list1,NavBarLocators.list_txt)
+        headers.clicking(NavBarLocators.header2, NavBarLocators.list1, NavBarLocators.list_txt)
+
+    def test1(self):
+        pass
+
+    def test2(self):
+        pass
+
+    def test3(self):
+        pass
--- Web/Tests/Chrome_Tests/test_commonQue.py
+++ Web/Tests/Chrome_Tests/test_commonQue.py
@@ -1,15 +1,15 @@
 import allure
 import pytest
 from Web.Pages.commonQuestion_Page import CommonQuePage
-from Web.Utils.PreConditions.precondition_betty import Pre_Condition_Betty
+from Web.Utils.PreConditions.precondition_tsiona import Pre_Condition_Tsiona
 
 
-@pytest.mark.usefixtures('login_correctly')
-class Test(Pre_Condition_Betty):
+@pytest.mark.usefixtures('login')
+class Test(Pre_Condition_Tsiona):
 
     @allure.description('Upload a product when the user does not have a store')
     @allure.severity(allure.severity_level.CRITICAL)
-    def test_moving_to_CommonQuPage(self, login_correctly):
+    def test_moving_to_CommonQuPage(self):
 
         """The Driver And The PreCondition("Login Correctly") - What is the Requirements for ?"""
         driver = self.driver
