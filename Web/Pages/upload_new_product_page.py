5b509555729948b0e0358971949ce03055a4a99d
Your Name <you@example.com>
2022-06-21 13:35:47 +0300

    mark-sanity added to the tests mark-regression added to the tests steps added to page - Jenkins -

--- Web/Pages/upload_new_product_page.py
+++ Web/Pages/upload_new_product_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.chrome.webdriver import WebDriver
 from Web.Utils.utils import Utils
 from selenium.webdriver.common.by import By
@@ -17,6 +18,7 @@ class Upload_New_Product_Page:
         self.validationFailed = Upload_New_Product_Locators.STORE_VALIDATION_FAILED  # Error Message (No Store)
         self.fieldError = Upload_New_Product_Locators.FILL_THE_FIELD  # Error Message for all The Fields
 
+    @allure.step
     def click_add_new_product_section(self):
         wait = WebDriverWait(self.driver, 20)
         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, self.addNewProductSection)))
@@ -30,6 +32,7 @@ class Upload_New_Product_Page:
         utils.validation(len(fields), 8)
         return fields
 
+    @allure.step
     def click_add_new_product_button(self):
         utils = Utils(self.driver)
         wait = WebDriverWait(self.driver, 20)
@@ -38,6 +41,7 @@ class Upload_New_Product_Page:
         utils.validation(button.get_attribute('defaultValue'), 'הוספה')
         button.click()
 
+    @allure.step
     def enter_data_to_inputs(self, data: list):
         utils = Utils(self.driver)
         fields = self.inputs_fields()
@@ -50,15 +54,19 @@ class Upload_New_Product_Page:
             raise ValueError
 
     """ Messages: """
+
+    @allure.step
     def js_messages_for_all_the_fields(self, num):
         fields = self.inputs_fields()
         return fields[num].get_attribute('validationMessage')
 
+    @allure.step
     def store_validation_failed_message(self):
         wait = WebDriverWait(self.driver, 20)
         wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, self.validationFailed)))
         return self.driver.find_element(By.CSS_SELECTOR, self.validationFailed).get_attribute('innerText')
 
+    @allure.step
     def enter_the_field_message(self, num):
         messages = self.driver.find_elements(By.XPATH, self.fieldError)
         return messages[num].get_attribute('innerText')
@@ -88,6 +96,7 @@ class Upload_Product_with_Store:
         self.amountOfDays = Upload_New_Product_Locators.AMOUNT_OF_DAYS  # The Total Of Business Days
         self.fieldMessages = Upload_New_Product_Locators.ERROR_FIELD_IS_EMPTY
 
+    @allure.step
     def click_add_new_product_section(self):
         wait = WebDriverWait(self.driver, 20)
         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, self.addNewProductSection)))
@@ -101,6 +110,7 @@ class Upload_Product_with_Store:
         utils.validation(len(fields), length)
         return fields
 
+    @allure.step
     def click_add_new_product_button(self):
         utils = Utils(self.driver)
         wait = WebDriverWait(self.driver, 20)
@@ -109,6 +119,7 @@ class Upload_Product_with_Store:
         utils.validation(button.get_attribute('defaultValue'), 'הוספה')
         button.click()
 
+    @allure.step
     def enter_data_to_inputs(self, length: int, data: list):
         utils = Utils(self.driver)
         fields = self.inputs_fields(length)
@@ -120,16 +131,19 @@ class Upload_Product_with_Store:
         else:
             raise ValueError
 
+    @allure.step
     def click_on_upload_image(self, path):
         image = self.driver.find_element(By.XPATH, self.uploadImage)
         image.send_keys(path)
 
+    @allure.step
     def fill_checkbox(self):
         utils = Utils(self.driver)
         checkbox = self.driver.find_element(By.XPATH, self.fillingCheckBox)
         checkbox.click()
         utils.validation(checkbox.is_selected(), True)
 
+    @allure.step
     def filter_products(self, choice: int):
         utils = Utils(self.driver)
         div = self.driver.find_elements(By.XPATH, self.unitsOrWeight)
@@ -142,6 +156,7 @@ class Upload_Product_with_Store:
         else:
             raise ValueError
 
+    @allure.step
     def click_plus_button(self, num):
         for i in range(num):
             wait = WebDriverWait(self.driver, 20)
@@ -149,6 +164,7 @@ class Upload_Product_with_Store:
             button = self.driver.find_element(By.XPATH, self.plusButton)
             button.click()
 
+    @allure.step
     def click_minus_button(self, num):
         for i in range(num):
             wait = WebDriverWait(self.driver, 20)
@@ -156,19 +172,24 @@ class Upload_Product_with_Store:
             button = self.driver.find_element(By.XPATH, self.minusButton)
             button.click()
 
+    @allure.step
     def amount_of_days(self):
         return self.driver.find_element(By.XPATH, self.amountOfDays).get_attribute('valueAsNumber')
 
     '''Messages'''
+
+    @allure.step
     def js_messages_for_all_the_fields(self, length, num):
         fields = self.inputs_fields(length)
         return fields[num].get_attribute('validationMessage')
 
+    @allure.step
     def upload_prod_successfully(self):
         wait = WebDriverWait(self.driver, 20)
         wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, self.validation)))
         return self.driver.find_element(By.CSS_SELECTOR, self.validation).get_attribute('textContent')
 
+    @allure.step
     def error_message(self, num):
         messages = self.driver.find_elements(By.CSS_SELECTOR, self.fieldMessages)
         return messages[num].get_attribute('innerText')
